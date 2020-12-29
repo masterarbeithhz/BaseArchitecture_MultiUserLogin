a073b5788e5407ecf554afbf005b093fe3ec0879
Tobias <masterarbeithhz@gmail.com>
2020-12-29 15:53:01 +0100

    sdf

--- image_files/index.php
+++ image_files/index.php
@@ -1,10 +1,11 @@
 <?php 
-    include('functions.php');
+/*    include('functions.php');
     if (!isLoggedIn()) {
         $_SESSION['msg'] = "You must log in first";
         header('location: login.php');
-    }
+    }*/
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
